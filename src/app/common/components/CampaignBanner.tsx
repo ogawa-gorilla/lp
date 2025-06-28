37e10fa85bce5145cf7927debd6d31788f4183f8
Gosuke Ogawa <ogawa.gousuke@gmail.com>
2025-06-28 19:31:25 +0900

    文言を訂正

--- src/app/common/components/CampaignBanner.tsx
+++ src/app/common/components/CampaignBanner.tsx
@@ -4,7 +4,7 @@ export default function CampaignBanner() {
     return (
         <div className="info-bar">
             🎉 モニターキャンペーン実施中！ <br />
-            初期費用+3ヶ月間保守費用無料！
+            初期費用無料+3ヶ月間保守費用無料！
             <a href="/monitor_campaign">詳しくはこちら</a>
         </div>
     )
